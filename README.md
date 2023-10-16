a82deb97d567b69842b7a6500b647cbd8cc7e83d
Yue Li <michaelli0914@outlook.com>
2023-10-16 18:58:34 +0000

    add caesar-cipher-cli

--- README.md
+++ README.md
@@ -9,21 +9,38 @@ This repository includes the main tasks for Week 7:
 * `Workflows` includes GitHub Actions, which contain configuration files for setting up automated build, test, and deployment pipelines for your project.
 * `.gitignore` is used to specify which files or directories should be excluded from version control when using Git.
 * `README.md` is the instruction file for the readers.
-* `main.py` is a Python file that contains the main function.
-* `test_main.py`  is a test file for `main.py` that can successfully run in IDEs.
-* `requirements.txt` is to specify the dependencies (libraries and packages) required to run the project.
+* `Cargo.toml`
+* `lib.rs`
+* `main.rs`
+* `test.rs`
 
 ## Project description
 
-* Package a Python script with setuptools 
+* Package a Python script with setuptools. I focus on building a Rust command line tool.
 * Include a user guide on usage and installation
-* Communicate with a database
+
 
 ## Project environment
 
 * Use codespace for scripting
-* Container built in `devcontainers` and virtual environment activated via `requirements.txt`
-* To run the code, use the command `python main.py` in the terminal
+* Set up for Rust, install Rust and Cargo, and check the versions
+* Run `cargo build` to compile the changes and `cargo run` to test the modified tool.
+
+## Project Guide
+
+* Select the folder `caesar-cipher-cli` from the course repo.
+* Create `main.rs` and `lib.rs` under the `src` file.
+* Create a function `int_to_ascii` in `lib.rs` to encode a number to a corresponding character by ASCII rule.
+* Add a command line to `int_to_ascii` to fit the new function.
+* Create a function `read_input` to accept file format input with `.txt` file.
+* Create `test.rs` to test the written functions.
+
+Description:
+* We can encrypt a string: `cargo run -- --message "string" --encrypt --shift "shift value"`.
+* We can decrypt a string: `cargo run -- --message "string" --decrypt --shift "shift value"`.
+* We can transfer an integer value to a corresponding character by ASCII rule: `cargo run -- --int-to-ascii --message int`.
+* We can change the content after message to `.txt` file: `cargo run -- --encrypt --message test.txt --shift 3` and `cargo run -- --int-to-ascii --message test.txt`.
+
 
 ## Check format & errors
 
---
+++ caesar-cipher-cli/src/main.rs
@@ -0,0 +1,90 @@
+/*
+
+To encrypt:
+
+cargo run --  --message "Off to the bunker. Every person for themselves" --encrypt --shift 10
+
+To decrypt:
+
+cargo run --  --message "Ypp dy dro lexuob. Ofobi zobcyx pyb drowcovfoc" --decrypt --shift 10
+
+To transfer a int value to a correspond character in ASCII table, replace "int" with your number:
+
+cargo run -- --int-to-ascii --message int
+
+*/
+
+use caeser_cipher_cli::{decrypt, encrypt, int_to_ascii};
+use clap::Parser;
+use std::fs;
+
+/// CLI tool to encrypt and decrypt messages using the caeser cipher
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Encrypt the message
+    #[arg(short, long)]
+    encrypt: bool,
+
+    /// decrypt the message
+    #[arg(short, long)]
+    decrypt: bool,
+
+    /// int to assii
+    #[arg(short, long)]
+    int_to_ascii: bool,
+
+    /// The message to encrypt, decrypt or transfer
+    #[arg(short, long)]
+    message: String,
+
+    /// The shift to use for the cipher
+    /// Must be between 1 and 25, the default is 3
+    #[arg(short, long, default_value = "3")]
+    shift: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let input_data = match read_input(&args.message) {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("Error reading input: {}", err);
+            return;
+        }
+    };
+
+    if args.encrypt {
+        let encrypted = encrypt(&input_data, args.shift);
+        println!("Encrypted message: {}", encrypted);
+    } else if args.decrypt {
+        let decrypted = decrypt(&input_data, args.shift);
+        println!("Decrypted message: {}", decrypted);
+    } else if args.int_to_ascii {
+        if let Ok(integer_value) = input_data.parse::<u8>() {
+            match int_to_ascii(integer_value) {
+                Ok(ascii_char) => {
+                    println!("Integer: {} corresponds to ASCII character: {}", integer_value, ascii_char);
+                }
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                }
+            }
+        } else {
+            eprintln!("Error: Invalid integer value provided.");
+        }
+    } else {
+        eprintln!("Please specify either --encrypt, --decrypt, or --int-to-ascii");
+    }
+}
+
+fn read_input(input: &str) -> Result<String, std::io::Error> {
+    if input.ends_with(".txt") {
+        // Read from a ".txt" file
+        fs::read_to_string(input)
+    } else {
+        // Otherwise, treat it as a direct input
+        Ok(input.to_string())
+    }
+}
